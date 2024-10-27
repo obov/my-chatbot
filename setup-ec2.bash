effc0944a63bf24f1a42c622474ff014855bf62f
obov <59566239+obov@users.noreply.github.com>
2024-10-28 00:45:17 +0900

    fix: add exit after bash

--- setup-ec2.bash
+++ setup-ec2.bash
@@ -6,7 +6,7 @@ case $ACTION in
     deploy)
         # 의존성 설치
         echo "의존성을 설치합니다..."
-        pip install -r requirements.txt
+        pip install -r requirements.txt --break-system-packages
 
         # 기존에 실행 중인 앱이 있으면 종료
         echo "기존 앱을 종료합니다..."
@@ -15,17 +15,17 @@ case $ACTION in
         # 앱 실행
         echo "앱을 백그라운드에서 실행합니다..."
         nohup streamlit run app.py --server.port 8501 --server.headless true &
-        disown
 
         echo "배포 작업이 완료되었습니다."
+        exit
         ;;
     start)
         # 앱 실행
         echo "앱을 백그라운드에서 실행합니다..."
         nohup streamlit run app.py --server.port 8501 --server.headless true &
-        disown
 
         echo "시작 후 설정 작업이 완료되었습니다."
+        exit
         ;;
     stop)
         # 앱 종료
@@ -33,6 +33,7 @@ case $ACTION in
         pkill -f 'streamlit run app.py'
 
         echo "종료 전 설정 작업이 완료되었습니다."
+        exit
         ;;
     *)
         echo "사용법: $0 {deploy|start|stop}"
