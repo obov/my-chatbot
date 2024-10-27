0e95cc706ad3f6b247bf93d09a5de2bd978a9378
obov <59566239+obov@users.noreply.github.com>
2024-10-27 22:04:14 +0900

    fix: some deploy errors

--- setup-ec2.bash
+++ setup-ec2.bash
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# 개발폴더로 이동
-cd /dev/doorfe
-
 # 패키지 설치
-pip3 install -r requirements.txt || pip install -r requirements.txt
+pip3 install -r requirements.txt --break-system-packages || pip install -r requirements.txt --break-system-packages
